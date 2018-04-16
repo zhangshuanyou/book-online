77af76af8ea2c072853678647c7ee9ccf4aab1a3
Zhangsy <1498317809@qq.com>
2018-04-16 15:42:30 +0800

    配置所需要的基本结构

---
+++ src/components/page/personal-page/PersonalPage.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="hello">
+    <header>个人</header>
+    <div class="header"></div>
+      {{ msg }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Personal',
+  data () {
+    return {
+       msg:'444444'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+@import '../../common/common.sass'
+</style>
